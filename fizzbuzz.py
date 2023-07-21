e9931e0bb7ef43e86fa23aff4e611545b1379c3b
puneets79 <puneet.sharma@credit-suisse.com>
2023-07-21 15:43:21 +0100

    Fizz buzz v1.1 - Using Dictionaries and allowing user to enter a max number

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,30 @@
-for number in range(1,100):
+my_dict = {3:"Fizz",5:"Buzz",7:"Bang",11:"Bong",13:"Fezz",17:""}
+input_text = input("max number ? ")
+max_number = int(input_text)
+for number in range(1,max_number):
     text = ""
+    multiples = []
+    if (number % 13) == 0:
+        multiples.append(13)
     if (number % 11) == 0:
-        text += "Bong"
+        multiples.append(11)
     if (number % 7 == 0):
-        text += "Bang"
+        multiples.append(7)
     if (number % 5 == 0):
-        text += "Buzz"
+        multiples.append(5)
     if (number % 3 == 0):
-        text += "Fizz"
-    if (text == ""):
+        multiples.append(3)
+    if (number % 17 == 0):
+        multiples.append(17)
+    if (multiples.count(17) > 0):
+        multiples.sort(reverse=True)
+    else:
+        multiples.sort()
+    
+    if (len(multiples) == 0):
         text = str(number)
+    else:
+        for a_number in multiples:
+            text+=my_dict[a_number]
+
     print (text)
